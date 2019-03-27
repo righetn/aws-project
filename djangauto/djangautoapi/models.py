f4e9dfb417cd5ca2e008e2a9afd200f7ef3c54b6
Nicolas Mulet <nicolas.mulet@epita.fr>
2019-03-27 17:24:06 +0100

    Add Models and start Admin

--- djangauto/djangautoapi/models.py
+++ djangauto/djangautoapi/models.py
@@ -5,7 +5,13 @@ class Car(models.Model):
     brand = models.CharField(max_length=200)
     creation_date = models.DateTimeField('creation date')
 
+    def __str__(self):
+        return self.brand
+
 
 class CarModel(models.Model):
     car = models.ForeignKey(Car, on_delete=models.CASCADE)
     model_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.model_name + str(car)
