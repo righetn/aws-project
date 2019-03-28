794627156860a07131cbcf4a51e4b65938680f07
Nicolas Righetti <nicolas.righetti@gmail.com>
2019-03-28 12:38:29 +0100

    change path id to brand and model name

--- djangauto/djangautoapi/urls.py
+++ djangauto/djangautoapi/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="Cars"),
-    path('<int:car_id>/', views.brand_detail, name="brand_detail"),
-    path('<int:car_id>/<int:model_id>/', views.model_detail, name="model_detail"),
+    path('<slug:brand>/', views.brand_detail, name="brand_detail"),
+    path('<slug:brand>/<slug:model_name>/', views.model_detail, name="model_detail"),
     path('addbrand/', views.add_brand, name="add_brand"),
-    path('<int:car_id>/addmodel/', views.add_model, name="add_model"),
+    path('<slug:brand>/addmodel/', views.add_model, name="add_model"),
 ]
--- djangauto/djangautoapi/views.py
+++ djangauto/djangautoapi/views.py
@@ -13,22 +13,20 @@ def index(request):
     context = {'car_list': car_list}
     return render(request, 'djangautoapi/index.html', context)
 
-def brand_detail(request, car_id):
+def brand_detail(request, brand):
     try:
-        car = Car.objects.get(pk = car_id)
-        carmodel_list = CarModel.objects.filter(car = car_id).order_by('model_name')
+        car = Car.objects.get(brand = brand)
+        carmodel_list = CarModel.objects.filter(car = car.id).order_by('model_name')
         context = {'car': car, 'carmodel_list': carmodel_list}
     except Car.DoesNotExist:
         raise Http404("Car does not exist")
     return render(request, 'djangautoapi/brand_detail.html', context)
 
-def model_detail(request, car_id, model_id):
+def model_detail(request, brand, model_name):
     try:
-        carmodel_list = CarModel.objects.filter(pk = model_id)
-        carmodel = carmodel_list[0]
-        brand = Car.objects.get(pk = car_id).brand
+        carmodel_list = CarModel.objects.filter(model_name = model_name)
+        carmodel = carmodel_list[0] 
 
-        model_name = carmodel.model_name
         directory = '../voitures/' + brand + '/' + model_name + '/'
         if not os.path.exists(directory):
             gis = GoogleImagesSearch('AIzaSyDL-iX9_5bYDWB5BHzXuMcV7xHt4_7X2JM', '003405953032685171249:uzag_hgt6fs')
@@ -57,9 +55,10 @@ def add_brand(request):
     else:
         return index(request)
 
-def add_model(request, car_id):
+def add_model(request, brand):
     try:
         model_name = request.POST['model_name']
+        car_id = Car.objects.get(brand = brand).id
         carmodel = CarModel(car = car_id, model_name = model_name)
         carmodel.save()
     except (Car.DoesNotExist):
