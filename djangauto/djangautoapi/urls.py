c392d84d9afb8db801e3c89f1c96cb8e5e699a45
Nicolas Mulet <nicolas.mulet@epita.fr>
2019-03-28 10:33:29 +0100

    Fix bug

--- djangauto/djangautoapi/urls.py
+++ djangauto/djangautoapi/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="Cars"),
     path('<int:car_id>/', views.brand_detail, name="brand_detail"),
-    path('<int:car_id>/<int:model_detail>', views.model_detail, name="model_detail")
+    path('<int:car_id>/<int:model_id>/', views.model_detail, name="model_detail")
 ]
