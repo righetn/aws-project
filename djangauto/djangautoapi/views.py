aa04909712c987cac39567a8a1d5d84055464881
Nicolas Mulet <nicolas.mulet@epita.fr>
2019-03-28 11:48:29 +0100

    Change redirection

--- djangauto/djangautoapi/views.py
+++ djangauto/djangautoapi/views.py
@@ -36,7 +36,7 @@ def add_brand(request):
     except (Car.DoesNotExist):
         raise Http404("Car does not exist")
     else:
-        return redirect(str(car.id) + "/")
+        return brand_detail(request, car.id)
 
 def add_model(request, car_id):
     try:
@@ -46,4 +46,4 @@ def add_model(request, car_id):
     except (Car.DoesNotExist):
         raise Http404("Car does not exist")
     else:
-        return redirect(str(car_id) + "/" + str(carmodel.id) + "/")
+        return model_detail(request, car_id, carmodel.id)
