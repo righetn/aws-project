6cbcef8e9fbea4423971e25c540205454a3ddfbe
Nicolas Righetti <nicolasrighetti@MacBook-Pro-de-Nicolas.local>
2019-05-23 16:12:02 +0200

    update model for concessionnary

--- djangauto/djangautoapi/models.py
+++ djangauto/djangautoapi/models.py
@@ -1,16 +1,24 @@
 from django.db import models
 
-
-class Car(models.Model):
-    brand = models.CharField(max_length=200, unique=True)
-    creation_date = models.DateTimeField('creation date')
+class CarBrand(models.Model):
+    name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
-        return self.brand.replace('_', ' ')
+        return self.name.__str__().replace('_', ' ')
 
 class CarModel(models.Model):
-    car = models.ForeignKey(Car, on_delete=models.CASCADE)
-    model_name = models.CharField(max_length=200)
+    name = models.CharField(max_length=30)
+    creation_date = models.DateTimeField('creation date')
+    brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE)
+    price = models.FloatField(null=False, blank=False, default=0)
+    stock = models.IntegerField(default=0)
 
     def __str__(self):
-        return str(self.car) + " " + self.model_name.replace('_', ' ')
+        return self.name.__str__().replace('_', ' ')
+
+class Car(models.Model):
+    model = models.ForeignKey(CarModel, on_delete=models.CASCADE)
+    brand = models.ForeignKey(CarBrand, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.brand.__str__() + " " + self.model.__str__()
