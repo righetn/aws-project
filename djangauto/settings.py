44acd3e08058c2a8114e12a23e15c29ba7c982f3
Nicolas Righetti <nicolasrighetti@MacBook-Pro-de-Nicolas.local>
2019-06-14 14:39:29 +0200

    check DATABASE_URL is defined

--- djangauto/settings.py
+++ djangauto/settings.py
@@ -78,12 +78,12 @@ WSGI_APPLICATION = "djangauto.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#   'default': {
-#      'ENGINE': 'django.db.backends.sqlite3',
-#     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-# }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+    }
+}
 
 
 # Password validation
@@ -135,4 +135,5 @@ LOGOUT_REDIRECT_URL = "/"
 
 django_heroku.settings(locals())
 
-DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+if "DATABASE_URL" in os.environ:
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
